dcd0a9eeff35ea6e2cee1eab4d71b51a4b786446
Harrylepap <rakotondratompobakoharry@yahoo.fr>
2019-01-05 15:39:26 +0300

    Exercice 2 (Question et Solutions)

---
+++ solutions/exercice2.py
@@ -0,0 +1,7 @@
+def factoriel(nombreDonnee):
+          if  nombreDonnee == 0 :
+                    return 1
+          return nombreDonnee * factoriel(nombreDonnee - 1)
+
+dataUtilisateur = int(input("Entrez un nombre = "))
+print(factoriel(dataUtilisateur))
